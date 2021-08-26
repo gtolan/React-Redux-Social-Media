99851dd26f702bd1c4a410496eead69a49281bd2
GTolan <tolangerard@gmail.com>
2021-08-26 11:03:13 +0100

    add loading screen component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,9 +3,13 @@ import SignUpButtons from '../components/SignUpButtons.js';
 import WelcomeBanner from '../components/WelcomeBanner.js';
 import birdIconBlue from '../styles/bird-blue.svg';
 import musicNoteBlue from '../styles/musical-note-blue.svg';
+import Loading from './Loading';
 
 const HomePage = () => {
     return (
+
+        <div>
+        <Loading />
         <div className="homepage-header">
             <div className="container-fluid p-4">
                 <div className="container-fluid bird-navbar">
@@ -23,6 +27,7 @@ const HomePage = () => {
                 <SignUpButtons/>
             </div>
             <WelcomeBanner/>
+        </div>
         </div>
     )
 }
---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+
+
+const Loading = () => {
+    return (
+        <div class="loading">
+            <p>Loading...</p>
+        </div>
+    )
+}
+
+export default Loading;
